5257ccbc9293bf0fa603314cc0fbdf4b7e5371a3
jaaziel.jimenez <jaaziel.jimenez@coppel.com>
2025-02-27 15:18:54 -0700

    Upload project

---
+++ src/main/java/com/unir/msthelibraryoperator/MsTheLibraryOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.unir.msthelibraryoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTheLibraryOperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTheLibraryOperatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unir/msthelibraryoperator/MsTheLibraryOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.msthelibraryoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTheLibraryOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
