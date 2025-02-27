f68d67c1309b516ceaafe24fb59b5c6a07a65640
jaaziel.jimenez <jaaziel.jimenez@coppel.com>
2025-02-27 16:01:35 -0700

    cambios

--- src/main/java/com/unir/msthelibraryoperator/config/AppConfig.java
+++ src/main/java/com/unir/msthelibraryoperator/config/AppConfig.java
@@ -1,4 +1,25 @@
 package com.unir.msthelibraryoperator.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class AppConfig {
+
+    private String authUri;
+    private boolean ignoreSession;
+    private String allowedOrigins;
+    private String allowedMethods;
+    private String allowedHeaders;
+    private String allowedPaths;
+    private boolean isProd;
+    private String correo;
+
 }
